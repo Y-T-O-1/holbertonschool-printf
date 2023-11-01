dedf60c2ee707bebb1fcc1f20673bf114b2002c9
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-01 18:46:38 -0500

    Fixed betty style in print_c.c

--- print_c.c
+++ print_c.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_c - A funtion that prints a single char
- * @c: character to print
+ * print_c - A funtion that prints a single char.
+ * @c: Char to print
  * Return: Always 1 (Success)
 */
+int print_c(va_list c)
 {
     char character = (char)va_arg(c, int);
+   
     _putchar(character);
-
-    return (1);
+     return (1);
 }
