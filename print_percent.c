3fbf837ff4fe8721efdfdf45536eaab5e93f23b1
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-01 16:46:26 -0500

    Add function to handle printing % symbol

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_percent - Print % symbol
+ * @percent: Argument passed to us (although it's unused in this function)
+ * Return: 1 if we're good, Else if not.
+ */
+int print_percent(__attribute__((unused))va_list percent)
+{
+	_putchar('%');
+	return (1);
+}
