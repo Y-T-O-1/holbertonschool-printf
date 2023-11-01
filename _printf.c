d04e65c001d274e49ce694e1370d9f4bfb522232
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-01 18:12:04 -0500

    Fixed betty style i printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,8 @@
  * check_format - checks if there is a valid format specifier
  * @format: possible valid format specifier
  * Return: pointer to valid function or NULL
- */
+*/
+
 int (*check_format(const char *format))(va_list)
 {
 	int i = 0;
