ef56116bcd7b785c19086aec0d758032acc19071
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-02 01:07:55 -0500

    Made changes to printf.c

--- _printf.c
+++ _printf.c
@@ -59,7 +59,7 @@ int _printf(const char *format, ...)
             }
             else if (format[i + 1] == '\0')
             {
-                return counter;
+                return (0);
             }
             else
             {
