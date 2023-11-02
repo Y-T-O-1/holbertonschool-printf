5b807c673b30db8fe69f6de6f1c007dc31978944
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-02 01:47:01 -0500

    Fixed betty style in _printf.c

--- _printf.c
+++ _printf.c
@@ -2,81 +2,81 @@
 #include <stdarg.h>
 
 /**
- * check_format - checks if there is a valid format specifier
- * @format: possible valid format specifier
- * Return: pointer to valid function or NULL
- */
+* check_format - checks if there is a valid format specifier
+* @format: possible valid format specifier
+* Return: pointer to valid function or NULL
+*/
 int (*check_format(const char *format))(va_list)
 {
-    print_t p[] = {
-        {"c", print_c},
-        {"s", print_s},
-        {"%", print_percent},
-        {NULL, NULL}
-    };
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
 
-    int i;
+	int i;
 
-    for (i = 0; p[i].t != NULL; i++)
-    {
-        if (*(p[i].t) == *format)
-            return (p[i].f);
-    }
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+			return (p[i].f);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
- * _printf - Function that mimics printf
- * @format: a string
- * Return: number of characters printed
- */
+* _printf - Function that mimics printf
+* @format: a string
+* Return: number of characters printed
+*/
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int i, counter = 0;
+	va_list ap;
+	int i, counter = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(ap, format);
+	va_start(ap, format);
 
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] != '%')
-        {
-            _putchar(format[i]);
-            counter++;
-        }
-        else
-        {
-            if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                counter++;
-                i++;
-            }
-            else if (format[i + 1] == '\0')
-            {
-                return (0);
-            }
-            else
-            {
-                int (*f)(va_list) = check_format(&format[i + 1]);
-                if (f == NULL)
-                {
-                    _putchar(format[i]);
-                    counter++;
-                }
-                else
-                {
-                    counter += f(ap);
-                    i++;
-                }
-            }
-        }
-    }
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			counter++;
+		}
+		else
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				counter++;
+				i++;
+			}
+			else if (format[i + 1] == '\0')
+			{
+				return (0);
+			}
+			else
+			{
+				int (*f)(va_list) = check_format(&format[i + 1]);
+				if (f == NULL)
+				{
+					_putchar(format[i]);
+					counter++;
+				}
+				else
+				{
+					counter += f(ap);
+					i++;
+				}
+			}
+		}
+	}
 
-    va_end(ap);
-    return counter;
+	va_end(ap);
+	return counter;
 }
