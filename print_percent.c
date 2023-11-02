469e3576589ca81b4740c3170a7c0f28ee7edd4b
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-01 20:17:51 -0500

    Fixed print_percent.c

--- print_percent.c
+++ print_percent.c
@@ -5,7 +5,7 @@
  * @percent: Argument passed to us (although it's unused in this function)
  * Return: 1 if we're good, Else if not.
  */
-int print_percent(__attribute__((unused))va_list percent)
+int print_percent(va_list percent)
 {
 	_putchar('%');
 	return (1);
