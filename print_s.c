56d2f9ef7fb366bc5f1db6c0fc737f8c43501403
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-02 03:35:19 -0500

    Betty doc changes print_s.c

--- print_s.c
+++ print_s.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * print_s - Prints string
- * @s: Arguments
- *
- * @Return: Number of char printed
- **/
+ * print_s - A function that prints a string
+ * @s: string to print
+ * Return: i
+ */
 int print_s(va_list s)
 {
-	/* Set our perameters */
 	char *str = va_arg(s, char *);
 	int i = 0;
 
