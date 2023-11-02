2cc0586b3f84a3f245d63b1fd9594f31324b7483
Y-T-O-1 <7198@holbertonstudents.com>
2023-11-02 02:17:10 -0500

    Fixed betty code in print_s.c

--- print_s.c
+++ print_s.c
@@ -4,10 +4,12 @@
 /**
 * print_s - Prints string
 * @s: Arguments
+*
 * @Return: Number of char printed
 */
 int print_s(va_list s)
 {
+	/* Set our perameters */
 	char *str = va_arg(s, char *);
 	int i = 0;
 
