e8bb7b0d9795559e1d6545cdccd40375e3e93a7c
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-06 18:45:35 -0600

    Added code documentation

--- print_s.c
+++ print_s.c
@@ -2,18 +2,30 @@
 #include <unistd.h>
 
 /**
- * print_s - A function that prints a string
- * @s: string to print
- * Return: i
+ * print_s - A function that prints a string.
+ * @s: string to print passed as part of a variable arguments list
+ * Return: The number of characters printed.
  */
 int print_s(va_list s)
 {
+	/* Retrieve the next argument (a string) from the va_list */
 	char *str = va_arg(s, char *);
+	/* Initialize a counter for the number of characters printed */
 	int i = 0;
 
+	/* If the string is NULL, set it to print "(null)" instead */
 	if (str == NULL)
+	{
 		str = "(null)";
+	}
+
+	/* Loop through the string and print each character until the null-terminator is encountered */
 	while (str[i])
+	{
+		/* Print current character and increment the print counter */
 		_putchar(str[i++]);
+	}
+
+	/* Return the number of characters printed */
 	return (i);
 }
